2dee71b0915d6201ccd59976b0bd9a57777db3e8
artem <127351345+talafarael@users.noreply.github.com>
2024-12-25 16:28:32 +0200

    login

--- src/Molecules/LoginInput/index.tsx
+++ src/Molecules/LoginInput/index.tsx
@@ -4,20 +4,29 @@ import React from "react";
 import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { dataLoginInput } from "./LoginInput.data";
+import { ILogin } from "type/ILogin";
+import { useMutationLogin } from "api/authApi/useQueryLogin";
 
 export default function LoginInput() {
   const navigate = useNavigate();
-  const { handleSubmit, control } = useForm<>({
+  const { mutate: login, isLoading, isError, data } = useMutationLogin();
+  const { handleSubmit, control } = useForm<ILogin>({
     defaultValues: {
       email: "",
       password: "",
     },
   });
-  const onSubmit = async (data) => {
+  const onSubmit = async (data: ILogin) => {
     const body = {
       email: data.email,
       password: data.password,
     };
+    login(body, {
+      onSuccess: (data) => {
+        localStorage.setItem("token", data.token);
+        
+      },
+    });
   };
   return (
     <form className="RegisterInput" onSubmit={handleSubmit(onSubmit)}>
--- src/api/authApi/useQueryLogin.ts
+++ src/api/authApi/useQueryLogin.ts
@@ -1,12 +1,19 @@
 import axios from "axios";
 import { useMutation } from "react-query";
+import { ILogin } from "type/ILogin";
+import { IToken } from "type/IToken";
 
-export const useLogin = async (body: any) => {
-  axios.post("http://localhost:5000/auth/login", body);
+export const useLogin = (body: ILogin): Promise<IToken> => {
+  return axios
+    .post<IToken>("http://localhost:5000/auth/login", body)
+    .then((res) => {
+      console.log(res)
+      return res.data;
+    });
 };
+
 export const useMutationLogin = () => {
-  return useMutation({
-    mutationKey: ["login"],
-    mutationFn: (body: any) => useLogin(body),
+  return useMutation<IToken, Error, ILogin>({
+    mutationFn: useLogin,
   });
 };
